c298d7424908c1db36334e661cc24b759db66f10
wthacher <wthacher@email.sc.edu>
2025-03-16 23:38:16 -0400

    aaa

--- proj2_25sp/proj2/src/bool_expr_client.cc
+++ proj2_25sp/proj2/src/bool_expr_client.cc
@@ -10,22 +10,7 @@ void BoolExprClient::Run() {
 
 // Main function
 int main(int argc, char* argv[]) {
-    if (argc < 3) {
-        std::cerr << "Usage: " << argv[0] <<
-        " <socket_path> <truth_values...>" << std::endl;
-        return 1;
-    }
-
-    // Extract the socket path and the truth values from command-line arguments
-    std::string socket_path = argv[1];
-    std::vector<std::string> truth_values;
-    for (int i = 2; i < argc; ++i) {
-        truth_values.push_back(argv[i]);
-    }
-
-    // Create and run the client
-    BoolExprClient client(socket_path, truth_values);
-    client.Run();
-
+    TestClient dsc(argv[1], argv[2], argv[3][0], argv[4][0]);
+    dsc.Run();
     return 0;
 }
